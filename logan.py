5e2ea6b9b0e13138f6f9bed2cf77d2c530d76cf7
lookbou56-a11y <lookbou56@gmail.com>
2026-03-27 03:10:41 +0000

    Update logan.py

--- logan.py
+++ logan.py
@@ -8,28 +8,29 @@ def home():
     <html>
     <head>
         <title>LOGAN EFB STORE</title>
+
         <style>
             body {
                 margin: 0;
-                font-family: Arial;
+                font-family: Arial, sans-serif;
                 text-align: center;
                 color: white;
                 height: 100vh;
 
-                background: linear-gradient(-45deg, #0f2027, #203a43, #2c5364, #1e3c72);
+                background: linear-gradient(-45deg, #1e3c72, #2a5298, #00c6ff, #0072ff);
                 background-size: 400% 400%;
-                animation: gradientBG 10s ease infinite;
+                animation: bgMove 10s ease infinite;
             }
 
-            @keyframes gradientBG {
+            @keyframes bgMove {
                 0% {background-position: 0% 50%;}
                 50% {background-position: 100% 50%;}
                 100% {background-position: 0% 50%;}
             }
 
             h1 {
-                font-size: 55px;
-                margin-top: 100px;
+                font-size: 60px;
+                margin-top: 120px;
                 font-weight: bold;
             }
 
@@ -40,9 +41,9 @@ def home():
 
             .btn {
                 display: inline-block;
-                padding: 15px 30px;
+                padding: 16px 35px;
                 margin: 10px;
-                font-size: 18px;
+                font-size: 20px;
                 border-radius: 10px;
                 text-decoration: none;
                 color: white;
@@ -57,5 +58,32 @@ def home():
                 background: #007BFF;
             }
 
-            .payment {
-                background: #6
+            .btn:hover {
+                transform: scale(1.1);
+                opacity: 0.9;
+            }
+        </style>
+    </head>
+
+    <body>
+
+        <h1>🔥 LOGAN EFB STORE 🔥</h1>
+        <p>Buy • Sell • Trade Accounts</p>
+
+        <a class="btn whatsapp"
+        href="https://chat.whatsapp.com/FnWdfggfsAgJbwDWhwIZuk">
+        Join WhatsApp Group 🚀
+        </a>
+
+        <br>
+
+        <a class="btn contact"
+        href="https://wa.me/231887935222?text=Hi%2C%20I%20saw%20your%20store%20and%20I%E2%80%99m%20interested%20in%20your%20services.%20Can%20you%20guide%20me%20on%20how%20to%20proceed%3F">
+        Contact Admin 💬
+        </a>
+
+    </body>
+    </html>
+    '''
+
+app.run(host="0.0.0.0", port=3000)
